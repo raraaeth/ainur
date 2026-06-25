948148395892d04eafc8d0078e4e7cf3d712eac6
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-25 15:59:35 +0700

    Add fetchTransactions function for API data fetching
    Implement fetchTransactions function to retrieve and process transaction data from API.

--- js/api.js
+++ js/api.js
@@ -6,8 +6,74 @@
 /* ===========================
    FETCH DATA
 =========================== */
+/* =====================================================
+   Finance Dashboard v1.0
+   File : api.js
+===================================================== */
 
+let transactions = [];
+
+/* ===========================
+   FETCH DATA
+=========================== */
+
+async function fetchTransactions(){
+
+    try{
+
+        const response = await fetch(CONFIG.API_URL);
+
+        if(!response.ok){
+
+            throw new Error(
+                "Failed to fetch data."
+            );
+
+        }
+
+        const data = await response.json();
+
+        transactions = data.map(item=>({
+
+            date : item.Tanggal,
+
+            type : item.Jenis,
+
+            category : item.Kategori,
+
+            description : item.Keterangan,
+
+            amount : toNumber(item.Nominal)
+
+        }));
+
+        return transactions;
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+        showError(error.message);
+
+        return [];
+
+    }
+
+}
 
 /* ===========================
    ERROR HANDLER
 =========================== */
+function showError(message){
+
+    console.error(
+
+        "Finance Dashboard :",
+
+        message
+
+    );
+
+}
