9506d5f8e36d9a62ebeb8bed5e4fcd2327b28420
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-25 15:54:30 +0700

    Add configuration for Finance Dashboard
    Added configuration details for Finance Dashboard including app name, version, and transaction types.

--- js/config.js
+++ js/config.js
@@ -6,3 +6,108 @@
 /* ===========================
    CONFIGURATION
 =========================== */
+/* =====================================================
+   Finance Dashboard v1.0
+   File : config.js
+===================================================== */
+
+
+/* ===========================
+   APP
+=========================== */
+
+const APP = {
+
+    NAME : "Finance Dashboard",
+
+    VERSION : "1.0.0"
+
+};
+
+
+/* ===========================
+   OPENSHEET
+=========================== */
+
+const CONFIG = {
+
+    SHEET_ID :
+
+    "18SyIR5CwehYNStVL5DKpx18yhBj7On8slqsQ4-p_s-M",
+
+    SHEET_NAME :
+
+    "transaksi",
+
+    API_URL :
+
+    "https://opensheet.elk.sh/18SyIR5CwehYNStVL5DKpx18yhBj7On8slqsQ4-p_s-M/transaksi"
+
+};
+
+
+/* ===========================
+   TRANSACTION TYPE
+=========================== */
+
+const TRANSACTION = {
+
+    INCOME :
+
+    "Masuk",
+
+    EXPENSE :
+
+    "Keluar"
+
+};
+
+
+/* ===========================
+   SPECIAL CATEGORY
+=========================== */
+
+const CATEGORY = {
+
+    WIFE :
+
+    "Jatah istri",
+
+    SAVING :
+
+    "Tabungan"
+
+};
+
+
+/* ===========================
+   MONTH
+=========================== */
+
+const MONTH = [
+
+    "Januari",
+
+    "Februari",
+
+    "Maret",
+
+    "April",
+
+    "Mei",
+
+    "Juni",
+
+    "Juli",
+
+    "Agustus",
+
+    "September",
+
+    "Oktober",
+
+    "November",
+
+    "Desember"
+
+];
