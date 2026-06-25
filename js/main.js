02c279eace8b7460411366d33453cb766d17a5b8
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-25 16:01:14 +0700

    Initialize app on DOMContentLoaded
    Add event listener for DOMContentLoaded to initialize app.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@
 /* ===========================
    INITIALIZE APP
 =========================== */
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    init
+
+);
+
+async function init(){
+
+    console.log(
+
+        APP.NAME,
+
+        APP.VERSION
+
+    );
+
+    await fetchTransactions();
+
+    console.table(transactions);
+
+}
