44867321fd9978a9d1ed25fbcc518f4ee2f6ee4a
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-25 19:02:34 +0700

    Implement createTableRow and updateTable functions
    Added createTableRow function to generate table rows for transactions.

--- js/table.js
+++ js/table.js
@@ -6,13 +6,62 @@
 /* ===========================
    FILTER
 =========================== */
-
+// SOON
 
 /* ===========================
    SEARCH
 =========================== */
 
+// SOON
 
 /* ===========================
    TABLE
 =========================== */
+
+function createTableRow(item){
+
+    return `
+
+    <tr>
+
+        <td>${formatDate(item.date)}</td>
+
+        <td>
+
+            <span class="badge ${item.type}">
+
+                ${item.type}
+
+            </span>
+
+        </td>
+
+        <td>${item.category}</td>
+
+        <td>${item.description}</td>
+
+        <td class="${item.type}">
+
+            ${formatCurrency(item.amount)}
+
+        </td>
+
+    </tr>
+
+    `;
+
+}
+
+function updateTable(){
+
+    const tbody = document.getElementById(
+        "transactionTable"
+    );
+
+    if(!tbody) return;
+
+    tbody.innerHTML = Finance.table
+        .map(createTableRow)
+        .join("");
+
+}
