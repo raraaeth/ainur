450b7691850601906d5dd758462f280bd046b218
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-25 16:34:16 +0700

    Add functions to update summary and saving cards

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,8 +6,38 @@
 /* ===========================
    SUMMARY CARD
 =========================== */
+function updateSummaryCard(){
 
+    const summary = Finance.summary;
+
+    document.getElementById("totalIncome").textContent =
+        formatCurrency(summary.income);
+
+    document.getElementById("totalExpense").textContent =
+        formatCurrency(summary.expense);
+
+    document.getElementById("remainingBalance").textContent =
+        formatCurrency(summary.balance);
+
+    document.getElementById("savingRate").textContent =
+        `${summary.savingRate.toFixed(1)}%`;
+
+}
 
 /* ===========================
    SAVING CARD
 =========================== */
+function updateSavingCard(){
+
+    const summary = Finance.summary;
+
+    document.getElementById("savingIncome").textContent =
+        formatCurrency(summary.savingIncome);
+
+    document.getElementById("savingExpense").textContent =
+        formatCurrency(summary.savingExpense);
+
+    document.getElementById("savingDifference").textContent =
+        formatCurrency(summary.savingDifference);
+
+}
