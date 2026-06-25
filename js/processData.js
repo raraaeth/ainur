bcff6478dc35c747031d97125c402973fd09ed47
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-25 16:07:05 +0700

    Implement getSummary function for transaction analysis
    Add getSummary function to calculate financial summary.

--- js/processData.js
+++ js/processData.js
@@ -6,7 +6,89 @@
 /* ===========================
    SUMMARY
 =========================== */
+function getSummary(){
 
+    let income=0;
+
+    let expense=0;
+
+    let savingIncome=0;
+
+    let savingExpense=0;
+
+    transactions.forEach(item=>{
+
+        if(item.type===TRANSACTION.INCOME){
+
+            income+=item.amount;
+
+        }
+
+        if(item.type===TRANSACTION.EXPENSE){
+
+            expense+=item.amount;
+
+        }
+
+        if(
+
+            item.type===TRANSACTION.INCOME &&
+
+            item.category===CATEGORY.SAVING
+
+        ){
+
+            savingIncome+=item.amount;
+
+        }
+
+        if(
+
+            item.type===TRANSACTION.EXPENSE &&
+
+            item.category===CATEGORY.SAVING
+
+        ){
+
+            savingExpense+=item.amount;
+
+        }
+
+    });
+
+    const balance=
+
+    income-expense;
+
+    const savingRate=
+
+    income===0
+
+    ?0
+
+    :((balance/income)*100);
+
+    return{
+
+        income,
+
+        expense,
+
+        balance,
+
+        savingRate,
+
+        savingIncome,
+
+        savingExpense,
+
+        savingDifference:
+
+        savingIncome-savingExpense
+
+    };
+
+}
 
 /* ===========================
    CATEGORY
