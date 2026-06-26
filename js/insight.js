c16d11afb13ca38c7a2e5d0a52673151074cd5fd
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-26 12:37:44 +0700

    Add createInsightItem and updateInsight functions

--- js/insight.js
+++ js/insight.js
@@ -1,8 +1,55 @@
+
 /* =====================================================
    Finance Dashboard v1.0
    File : insight.js
 ===================================================== */
 
 /* ===========================
-   INSIGHT ENGINE
+   CREATE INSIGHT
 =========================== */
+
+function createInsightItem(item){
+
+    return `
+
+    <div class="analytics-item">
+
+        <span>
+
+            ${item.icon}
+
+            ${item.text}
+
+        </span>
+
+    </div>
+
+    `;
+
+}
+
+/* ===========================
+   UPDATE INSIGHT
+=========================== */
+
+function updateInsight(){
+
+    const container =
+
+    document.getElementById(
+
+        "insightContainer"
+
+    );
+
+    if(!container) return;
+
+    container.innerHTML =
+
+    Finance.insight
+
+    .map(createInsightItem)
+
+    .join("");
+
+}
