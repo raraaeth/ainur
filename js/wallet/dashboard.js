dfe984fde4703d27e9a4892eeba3cb2025c9ff76
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-28 12:49:05 +0700

    Add functions to get portfolio and core crypto values

--- js/wallet/dashboard.js
+++ js/wallet/dashboard.js
@@ -7,3 +7,36 @@ function getCryptoSummary(){
     return Wallet.summary;
 
 }
+
+/* =========================
+   GET PORTFOLIO
+========================= */
+
+function getPortfolioSummary(
+    portfolio
+){
+
+    return (
+
+        Wallet.summary
+        .portfolios[
+            portfolio
+        ] ||
+
+        null
+
+    );
+
+}
+
+/* =========================
+   GET CORE VALUE
+========================= */
+
+function getCoreCryptoValue(){
+
+    return Wallet.summary.coreUSD;
+
+}
+
+
