fd5ffb9c9c16fd9c2442429e646e7e51d54765cc
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-28 12:57:27 +0700

    Update Moralis API key in config.js

--- js/wallet/config.js
+++ js/wallet/config.js
@@ -33,7 +33,7 @@ const Wallet = {
 const MORALIS = {
 
     API_KEY:
-    "PASTE_API_KEY_KAMU_DISINI",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImI5Njg2MDYxLTM5MDUtNDhmYi1hMjNjLTE1M2ExMWVkYTcxOSIsIm9yZ0lkIjoiNTIxNTEzIiwidXNlcklkIjoiNTM2NzE5IiwidHlwZUlkIjoiY2ZhZWZiM2ItNjYzNS00Zjk5LWFiOTMtZWMxMDZmODEzMGMzIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3ODI2MTYyMzMsImV4cCI6NDkzODM3NjIzM30.NLTWyp4hjTLtEgb4HMfxhbadgW_6w6SudAwozrczi4o",
 
     BASE_URL:
     "https://deep-index.moralis.io/api/v2.2"
