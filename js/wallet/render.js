1c5a3b82cbf2875e77cddfc8e41d937ba121663c
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-28 14:34:55 +0700

    Add wallet dashboard rendering function

---
+++ js/wallet/render.js
@@ -0,0 +1,21 @@
+/* =========================
+   RENDER WALLET
+========================= */
+
+function updateWalletDashboard(){
+
+    if(!Wallet.summary){
+
+        return;
+
+    }
+
+    console.log(
+
+        "Wallet Dashboard",
+
+        Wallet.summary
+
+    );
+
+}
