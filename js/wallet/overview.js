2050a2868610d0892dda5cb0aca864456d26bb33
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-30 22:30:49 +0700

    Refactor wallet card rendering logic

--- js/wallet/overview.js
+++ js/wallet/overview.js
@@ -29,50 +29,136 @@ Object.entries(
         return;
 
     }
-
+/*-------container inner html*/
     container.innerHTML =
 
-    portfolios.map(item => `
+portfolios.map(item=>{
 
-        <div class="wallet-card">
+const evm =
 
-            <div class="wallet-card-top">
+item.evm
 
-                <div>
+.map(token=>`
 
-                    <div class="wallet-name">
+<div class="wallet-token">
 
-                        ${item.name}
+    <span>${token.symbol}</span>
 
-                    </div>
+    <span>$${token.usd_value.toFixed(2)}</span>
 
-                    <div class="wallet-provider">
+</div>
 
-                        Multi Chain Wallet
+`)
 
-                    </div>
+.join("");
 
-                </div>
+const sol =
+
+item.sol
+
+.map(token=>`
+
+<div class="wallet-token">
+
+    <span>${token.symbol}</span>
+
+    <span>$${token.usd_value.toFixed(2)}</span>
+
+</div>
+
+`)
+
+.join("");
+
+return `
+
+<div class="wallet-card">
+
+    <div class="wallet-card-top">
+
+        <div>
+
+            <div class="wallet-name">
+
+                ${item.name}
 
             </div>
 
-            <div class="wallet-total">
+            <div class="wallet-provider">
 
-                $${item.totalUSD.toFixed(2)}
-
-            </div>
-
-            <div class="wallet-idr">
-
-                ≈ ${formatCurrency(
-                    item.totalUSD * USD_IDR
-                )}
+                Multi Chain Wallet
 
             </div>
 
         </div>
 
-    `).join("");
+    </div>
+
+    <div class="wallet-total">
+
+        $${item.totalUSD.toFixed(2)}
+
+    </div>
+
+    <div class="wallet-idr">
+
+        ≈ ${formatCurrency(
+            item.totalUSD * USD_IDR
+        )}
+
+    </div>
+
+    <div class="wallet-detail">
+
+        ${
+        evm
+        ?
+
+        `
+
+        <div class="wallet-network">
+
+            EVM
+
+        </div>
+
+        ${evm}
+
+        `
+
+        :
+
+        ""
+        }
+
+        ${
+        sol
+        ?
+
+        `
+
+        <div class="wallet-network">
+
+            SOLANA
+
+        </div>
+
+        ${sol}
+
+        `
+
+        :
+
+        ""
+        }
+
+    </div>
+
+</div>
+
+`;
+
+}).join("");
 
 }
 
