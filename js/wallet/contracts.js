1aeb1e62488fac28659c163273108701f90aed65
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-30 19:54:40 +0700

    Add core asset contract definitions
    Define core asset contracts for multiple blockchains including Ethereum, BSC, Base, Arbitrum, and Optimism.

---
+++ js/wallet/contracts.js
@@ -0,0 +1,67 @@
+/* =====================================================
+   CORE ASSET CONTRACT
+===================================================== */
+
+const CONTRACTS = {
+
+    ethereum:{
+
+        native:"ETH",
+
+        stable:{
+
+            USDC:
+            "0xA0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+
+            USDT:
+            "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+
+        }
+
+    },
+
+    bsc:{
+
+        native:"BNB",
+
+        stable:{
+
+            USDC:
+            "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+
+            USDT:
+            "0x55d398326f99059fF775485246999027B3197955"
+
+        }
+
+    },
+
+    base:{
+
+        native:"ETH",
+
+        stable:{
+
+            USDC:
+            "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+
+        }
+
+    },
+
+    arbitrum:{
+
+        native:"ETH",
+
+        stable:{
+
+            USDC:
+            "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+
+        }
+
+    },
+
+    optimism:{
+
+       
