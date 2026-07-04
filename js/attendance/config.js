be18f1e5e380fe4902786a1e28dc5bf39ca9decc
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-07-04 21:15:45 +0700

    Add sheetUrl to config.js
    Added sheetUrl for attendance data retrieval.

--- js/attendance/config.js
+++ js/attendance/config.js
@@ -16,6 +16,10 @@ const Attendance = {
 
     scriptUrl:
 
-    "https://script.google.com/macros/s/AKfycbxp3uIakJzUSNPLWuHWvyRh8MVAYEZEjN66nLI2kGXx7zwkKhfoGr0s1Z7fnx_KzLNR/exec"
+    "https://script.google.com/macros/s/AKfycbxp3uIakJzUSNPLWuHWvyRh8MVAYEZEjN66nLI2kGXx7zwkKhfoGr0s1Z7fnx_KzLNR/exec",
+
+    sheetUrl:
+
+    "https://opensheet.elk.sh/18SyIR5CwehYNStVL5DKpx18yhBj7On8slqsQ4-p_s-M/attendance"
 
 };
