b070fb92aeec45c3e515b364697b702059667319
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-07-04 20:25:34 +0700

    Add attendance configuration file

---
+++ js/attendance/config.js
@@ -0,0 +1,20 @@
+/* =====================================================
+   ATTENDANCE CONFIG
+===================================================== */
+
+const Attendance = {
+
+    raw: [],
+
+    data: [],
+
+    summary: {},
+
+    history: [],
+
+    current: {},
+
+    apiUrl:
+    "https://script.google.com/macros/s/AKfycbxp3uIakJzUSNPLWuHWvyRh8MVAYEZEjN66nLI2kGXx7zwkKhfoGr0s1Z7fnx_KzLNR/exec"
+
+};
