653521ae5af9c337c4f9e7d5d2c43a537ba12b63
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-07-04 20:39:02 +0700

    Add scriptUrl to attendance config

--- js/attendance/config.js
+++ js/attendance/config.js
@@ -14,7 +14,8 @@ const Attendance = {
 
     current: {},
 
-    apiUrl:
+    scriptUrl:
+
     "https://script.google.com/macros/s/AKfycbxp3uIakJzUSNPLWuHWvyRh8MVAYEZEjN66nLI2kGXx7zwkKhfoGr0s1Z7fnx_KzLNR/exec"
 
 };
