976ce15effa9f69000d89434112e6da9be68c739
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-07-05 19:33:34 +0700

    Update cache name to ainur-dashboard-v4

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    LIFE DASHBOARD PWA
 ===================================================== */
 
-const CACHE_NAME = "ainur-dashboard-v3";
+const CACHE_NAME = "ainur-dashboard-v4";
 
 const FILES = [
 
